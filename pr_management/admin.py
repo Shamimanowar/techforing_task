f393c6bbf6ed4a66a7de0230bd58d8b108e0faaa
shamim anowar <shamim.hire@gmail.com>
2025-07-12 23:08:54 +0600

    Register User model in admin.py

--- pr_management/admin.py
+++ pr_management/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Project, User
 
-# Register your models here.
+
+# Define the inlines
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 0
+    can_delete = False
+    
+    def has_change_permission(self, request, obj = None):
+        return super().has_change_permission(request, obj)
+    
+    
+# Register the models with the admin site
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_joined', 'last_login')
+    search_fields = ('username', 'email')
+    ordering = ['-date_joined', ]
+
+    inlines = [
+        ProjectInline,
+        ]
