b0520feb0cc541f795a4971861a4d974ef6ba3d4
shamim anowar <shamim.hire@gmail.com>
2025-07-13 22:30:23 +0600

    Add REST API endpoints for user, project, and related models

--- pr_management/views.py
+++ pr_management/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Project, ProjectMember, Task, Comment
+from .serializers import UserSerializer, ProjectSerializer, ProjectMemberSerializer, TaskSerializer, CommentSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+
+        # No validation is done here, keeping it open for simplicity
+        instance.set_password(self.request.data['password'])
+        instance.save()
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectMemberViewSet(viewsets.ModelViewSet):
+    queryset = ProjectMember.objects.all()
+    serializer_class = ProjectMemberSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
