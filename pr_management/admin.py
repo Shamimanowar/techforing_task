e9f525a366dbaebc7fa5a87aed29d76a37a14a87
shamim anowar <shamim.hire@gmail.com>
2025-07-13 22:42:58 +0600

    Register models to admin panel

--- pr_management/admin.py
+++ pr_management/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, User
+from .models import Project, User, ProjectMember, Task, Comment
 
 
 # Define the inlines
@@ -10,7 +10,23 @@ class ProjectInline(admin.TabularInline):
     
     def has_change_permission(self, request, obj = None):
         return super().has_change_permission(request, obj)
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 0
+    can_delete = True
     
+    def has_change_permission(self, request, obj = None):
+        return super().has_change_permission(request, obj)  
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    can_delete = True
+    
+    def has_change_permission(self, request, obj = None):
+        return super().has_change_permission(request, obj)
     
 # Register the models with the admin site
 @admin.register(User)
@@ -22,3 +38,37 @@ class UserAdmin(admin.ModelAdmin):
     inlines = [
         ProjectInline,
         ]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner__username', 'created_at')
+    search_fields = ('name', )
+    ordering = ('created_at', )
+    inlines = [
+        TaskInline,
+        ]
+    
+
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ('project__name', 'user__username', 'role',)
+    search_fields = ('project__name', 'user__username')
+    ordering = ['project__name', ]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'priority', 'assigned_to__username', 'project__name', 'created_at', 'due_date')
+    search_fields = ('title', 'description')
+    ordering = ['-created_at', ]
+    inlines = [
+        CommentInline,
+        ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'user__username', 'task__title', 'task__project__name', 'created_at')
+    search_fields = ('content', 'user__username')
+    ordering = ['-created_at', ]
