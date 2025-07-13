423acaf98d10537f3ad2d07d41714109e782826a
shamim anowar <shamim.hire@gmail.com>
2025-07-13 22:31:13 +0600

    Add app and API auth URLs to main routing

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -22,6 +22,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', include('pr_management.urls')),
+    
+    # Browseable API URLs
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 
